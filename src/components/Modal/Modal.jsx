b3b6b540b403eb3fcb44375c97167e8f3420f86d
John Dave Dalmao <johndavedalmao@gmail.com>
2024-12-14 15:44:35 +0800

    added the seminar details on the modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,8 @@ import { createPortal } from 'react-dom';
 import { default as certificateOfAppreciation } from '~/assets/img/software-engineer-portfolio-seminar/certificate-of-appreciation.png';
 import { default as seminarPoster } from '~/assets/img/software-engineer-portfolio-seminar/seminar-poster.png';
 import { ImageSlider } from '~/components/ImageSlider';
+import { Link } from '~/components/Link';
+import { Text } from '~/components/Text';
 import { tokens } from '~/components/ThemeProvider/theme';
 import { Transition } from '~/components/Transition';
 import { cssProps, msToNum, numToMs } from '~/utils/style';
@@ -24,6 +26,21 @@ export const Modal = ({ isOpen, onClose }) => {
             <button onClick={onClose}>close</button>
 
             <ImageSlider imageUrls={IMAGES} />
+
+            <Text className={styles.description} data-visible={visible} size="l" as="p">
+              On November 12, 2022, I conducted a seminar at University of Bohol focused
+              on building a software developer portfolio, covering everything from initial
+              setup to deployment.
+            </Text>
+
+            <Text className={styles.description} data-visible={visible} size="l" as="p">
+              I aimed to guide students in creating an effective portfolio that would help
+              them secure a software engineering job.
+            </Text>
+
+            <Link href="https://johndavedalmao-12nov2022.pages.dev/" target="_blank">
+              Click here to view the seminar's result.
+            </Link>
           </div>
         </div>
       )}
