0685724b82c71c745afa924b31351be2a32e4f52
John Dave Dalmao <johndavedalmao@gmail.com>
2024-12-26 15:43:01 +0800

    finalized the footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,17 +7,26 @@ import styles from './footer.module.css';
 export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
-      <span className={styles.date}>
-        {`© ${new Date().getFullYear()} ${config.name}.`}
-      </span>
-      <Link
-        secondary
-        className={styles.link}
-        href="https://github.com/davedalmao"
-        target="_blank"
-      >
+      <div className={styles.date}>{`© ${new Date().getFullYear()} ${config.name}.`}</div>
+    </Text>
+
+    <Text size="s" align="center">
+      <Link className={styles.link} href={`${config.github}/portfoliooo`} target="_blank">
         Crafted by yours truly
       </Link>
+
+      <span>
+        , inspired by{' '}
+        <Link
+          secondary
+          className={styles.link}
+          href="https://github.com/HamishMW/portfolio"
+          target="_blank"
+        >
+          Hamish Williams
+        </Link>
+        .
+      </span>
     </Text>
   </footer>
 );
