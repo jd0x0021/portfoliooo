03cb00b8bf1cbde9ca4372e6b5e1b35980bdcf8f
John Dave Dalmao <johndavedalmao@gmail.com>
2025-01-19 17:45:19 +0800

    updated vite config to not add a file has for assets that are in assets\resume directory

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,26 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '~', replacement: path.resolve(__dirname, './src') }],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // Custom asset filename configuration
+        assetFileNames: assetInfo => {
+          // Get the relative file path of an asset
+          // from the root directory (root = src folder)
+          const assetPath = assetInfo.originalFileName
+            ? path.relative(path.resolve(__dirname, 'src'), assetInfo.originalFileName)
+            : '';
+
+          // Don't add a file hash for assets in the assets\resume directory
+          if (assetPath.startsWith('assets\\resume')) {
+            return 'assets/resume/[name][extname]';
+          }
+
+          // Retain the file hash for other assets
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 });
