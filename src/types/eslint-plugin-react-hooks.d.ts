99bfbc20c503da8cda0fa4730a408914255ecea7
John Dave Dalmao <johndavedalmao@gmail.com>
2025-02-01 16:23:09 +0800

    enabled eslint.config.js, and added a types file

---
+++ src/types/eslint-plugin-react-hooks.d.ts
@@ -0,0 +1,5 @@
+/**
+ * This is to let typescript know that this module exists and suppress the warning in eslint.config.js file.
+ * The warning comes from this import in eslint.config.js: import reactHooks from 'eslint-plugin-react-hooks';
+ */
+declare module 'eslint-plugin-react-hooks';
